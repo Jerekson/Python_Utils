e9ccd5a6a8613038dad703473bfd60067b3cb17f
koceila haddouche <had.koceila@gmail.com>
2021-06-29 08:22:19 +0200

    B64 decode works

--- Convertisseur/B64.py
+++ Convertisseur/B64.py
@@ -8,8 +8,8 @@ if choix == "encode" or choix == "Encode":
 	pass
 elif choix == "decode" or choix == "Decode":
 	# texte : QmlsbHlPdXNz==
-	msg = raw_input('tapez un message a decoder\n')
-	print("Message original : " + base64.decodestring(msg))
+	msg = input('tapez un message a decoder\n')
+	print("Message original : " + str(base64.urlsafe_b64decode(msg),)) # base64.decodestring(msg)
 else:
 	print("Erreur lors de la saisie")
 
