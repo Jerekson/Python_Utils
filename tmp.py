36cf3dda9979e55cc06244e0be06738fa2de160b
Jerekson <had.koceila@gmail.com>
2025-10-07 20:56:37 +0200

    tmp modified

--- tmp.py
+++ tmp.py
@@ -1,5 +1,11 @@
 # from little_projects.pwd_store import main as pwdGen
-from little_projects.pwd_store.main import PWD_generator
+# from little_projects.pwd_store.main import PWD_generator
 
+'''
 newpwd = PWD_generator()
 print(newpwd.get_new_pwd())
+'''
+
+
+
+import little_projects.pwd_store
