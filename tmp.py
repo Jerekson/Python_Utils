373df6a82669771230e5930e61f0cd2bd1b673d0
Jerekson <had.koceila@gmail.com>
2025-10-07 14:03:17 +0200

    pwd_gen continue

--- tmp.py
+++ tmp.py
@@ -1,5 +1,5 @@
-# from Learning.new_exercices import lvl1_secure_password_generator as pwdGen
-from Learning.new_exercices.lvl1_secure_password_generator import PWD_generator
+# from little_projects.pwd_store import main as pwdGen
+from little_projects.pwd_store.main import PWD_generator
 
 newpwd = PWD_generator()
 print(newpwd.get_new_pwd())
