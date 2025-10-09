c57974766ac7644a42bcbadbea00330ca5e9f6d7
Jerekson <had.koceila@gmail.com>
2025-10-09 20:59:38 +0200

    update on laptop

--- tmp.py
+++ tmp.py
@@ -1,14 +1,4 @@
-# from little_projects.pwd_store import main as pwdGen
-# from little_projects.pwd_store.main import PWD_generator
-
-'''
-newpwd = PWD_generator()
-print(newpwd.get_new_pwd())
-'''
-
-
-
-from little_projects.pwd_store import PWD_generator, SQL_config
+from little_projects.pwd_store import PWD_generator,
 
 newpwd = PWD_generator.get_new_pwd("ask for new")
 print(newpwd)
