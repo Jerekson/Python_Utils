8357d94fd26eab29642fe1561d905c7c3cd9b8c8
koceila haddouche <had.koceila@gmail.com>
2025-10-23 18:40:39 +0200

    json

--- labo/test-module/test_json_module.py
+++ labo/test-module/test_json_module.py
@@ -1,29 +1,67 @@
 import json, os
+from pathlib import Path
 
-dir_path = os.path.dirname(__file__)+"/"
-json_file_name = "test_json_module.json"
-# Write in json file
-with open('properties.json') as json_file:
-    properties = json.load(json_file)
-    properties["StringProperty"] = "new value"
-    json.dump(properties, json_file)
-    
-print(properties)
+dir_path = Path(__file__).resolve().parent
+json_file_path = dir_path / "test_json_module.json"
 
-"""
-with open('data.json', 'r+') as f:
-    data = json.load(f)
-    data['id'] = 134 # <--- add `id` value.
-    f.seek(0)        # <--- should reset file position to the beginning.
-    json.dump(data, f, indent=4)
-    f.truncate()     # remove remaining part
-"""
+def get_json_file_data(file_path):
+    try:
+        content = file_path.read_text(encoding="utf-8")
+        data = json.loads(content)
+        return data
+    except Exception as e:
+        print(e)
 
+def set_default_data(file_path):
+    default_data = {
+        "default datas" : [
+            {
+                "Author":"moi",
+                "year":2025,
+                "my file path":str(json_file_path)
+            }
+        ]
+    }
+    try:
+        file_path.write_text(json.dumps(default_data, indent=4), encoding="utf-8")
+        print("file created and default data set")
+    except Exception as e:
+        print(e)
 
+def add_data(file_path):
+    current_data = get_json_file_data(file_path)
+    # set new data
+    current_data["new data"] = "test"
 
-# read properties.json 
-with open('properties.json') as json_file:
-    properties = json.load(json_file)
+    # save 
+    file_path.write_text(json.dumps(current_data, indent=4), encoding="utf-8")
 
-print(properties)
+def delete_data(file_path, data, suppr):
+    pass
+
+# check if file exist, if it is, get current data
+if json_file_path.exists():
+    data = get_json_file_data(json_file_path)
+else: # if not, create it and set default data 
+    data = set_default_data(json_file_path)
+
+# add datas
+data["new data"] = [{
+    "skey1":"test",
+    "skey2":"Hello"
+}]
+data["new data2"] = [{
+    "skey1":"test2",
+    "skey2":"Hello2"
+}]
+data["new data3"] = [{
+    "skey1":"test3",
+    "skey2":"Hello3"
+}]
+
+# delete some data
+delete_data(json_file_path, data, "new data2")
+
+# save new datas
+json_file_path.write_text(json.dumps(data, indent=4), encoding="utf-8")
 
--- tmp.py
+++
@@ -1,6 +0,0 @@
-from little_projects.pwd_store import PWD_generator, SQL_config, logging_package_setup
-import logging
-
-logging_package_setup(level=logging.DEBUG)
-generator = PWD_generator()
-sqltest = SQL_config()
