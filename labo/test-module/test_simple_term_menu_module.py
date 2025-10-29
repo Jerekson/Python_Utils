6f794967c81d5831d82601f90f227ffb8a3fddec
koceila haddouche <had.koceila@gmail.com>
2025-10-29 18:39:24 +0100

    test venv and simple term menu v2

--- labo/test-module/test_simple_term_menu_module.py
+++ labo/test-module/test_simple_term_menu_module.py
@@ -1,16 +1,19 @@
 from simple_term_menu import TerminalMenu
 
 
-def main():
+def simple_select_menu():
 	options = [
 	"Entry 1",
 	"Entry 2",
 	"Entry 3"
 	]
-	#terminal_menu = TerminalMenu(options)
-	#menu_entry_index = terminal_menu.show()
-	print("You have selected ?")
+	terminal_menu = TerminalMenu(options)
+	menu_entry_index = terminal_menu.show()
+	print(f"You have selected {options[menu_entry_index]} ?")
 
+def multiple_select_menu():
+	pass
 
 if __name__ == "__main__":
-	main()
+	simple_select_menu()
+	multiple_select_menu()
