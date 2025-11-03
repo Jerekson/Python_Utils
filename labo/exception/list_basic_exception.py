506fa5c8f6f999b34a1577180580778ef61cf512
Jerekson <had.koceila@gmail.com>
2025-11-03 13:30:01 +0100

    Exception update

--- labo/exception/list_basic_exception.py
+++ labo/exception/list_basic_exception.py
@@ -1,1 +1,26 @@
-pass
+
+
+def simple_int_except():
+	while True:
+		try:
+
+			x = int(input("enter a number : "))
+			break
+		except ValueError as e:
+			print(e)
+
+def dontunderstand():
+	try:
+	    raise Exception('spam', 'eggs')
+	except Exception as inst:
+	    print(type(inst))    # the exception type
+	    print(inst.args)     # arguments stored in .args
+	    print(inst)          # __str__ allows args to be printed directly,
+	                         # but may be overridden in exception subclasses
+	    x, y = inst.args     # unpack args
+	    print('x =', x)
+	    print('y =', y)
+
+if __name__ == "__main__":
+	# simple_int_except()
+	dontunderstand()
